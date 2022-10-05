b78fad468bc33abfee3fc54ba776db7ef842b04d
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-05 11:19:47 +0000

    4 zad koniec

--- kartaPracy1.py
+++ kartaPracy1.py
@@ -1,1 +1,23 @@
-?
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# a = int(input())
+# b = int(input())
+# print((a+b)**2)
+
+# a = int(input())
+# b = int(input())
+# print((a-b)**3)
+
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+
+# a = int(input())
+# print(a/1.23)
+
+# a = int(input())
+# b = int(input())
+# print(a%b)
+
---
+++ kartapracy2.py
@@ -0,0 +1,24 @@
+# a = int(input("podaj liczbe"))
+# if a % 3 == 0:
+#     print("tak")
+# else:
+#     print("nie")
+
+# a = int(input("podaj liczbe: "))
+# if a>=100 and a<1000 and a%17==0:
+#     print("tak")
+# else:
+#     print("nie")
+
+# wiek = int(input("ile masz lat?\n" ))
+# if wiek<15:
+#     print("prokurator cie dojedzie")
+# else:
+#     print("zabezpieczaj sie")
+
+LIMIT = 20
+waga = int(input("ile wazy tan gruzowóz?"))
+if waga<=LIMIT:
+    print("jedz pan")
+else:
+    print("stop")
