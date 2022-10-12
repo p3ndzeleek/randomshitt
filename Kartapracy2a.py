a1a5dae9581443f5f4225c8c8cbe879241205329
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-12 16:37:32 +0000

    będzie magik

---
+++ Kartapracy2a.py
@@ -0,0 +1,16 @@
+# #zad1
+# a = int(input())
+# b = int(input())
+# if (a+b) %2 ==0:
+#     print("Tak")
+# else:
+#     print("Nie")
+
+#zad2
+
+# a = int(input())
+# g = int(input())
+# if (a+g)/2 > (a**g)/(1/2):
+#     print("Tak")
+# else:
+#     print("Nie")
