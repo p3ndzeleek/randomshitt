0eab39b7e1f62d5b04933d32a427790305a017b6
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-12 11:20:24 +0000

    petle czy cos takiego

---
+++ Kartapracy3.py
@@ -0,0 +1,40 @@
+# for i in range(100):
+#     print( i, end=" ")
+
+#2 
+# for i in range(10,20):
+#     print(i, end=" ")
+
+#3
+# for i in range(15,33,2):
+#     print( i , end=" ")
+
+# #4
+
+# for i in range(99,9,-1):
+#     print(i, end=" ")
+
+
+# for i in range(99,9,1):
+#     print(i, end=" ")
+
+# #5
+
+# for i in range(-10,-100,-1):
+#     print(i, end=" ")
+
+# for i in range(100,1000,20):
+#     print(i, end=" ")
+
+# n = int(input())
+# for i in range(n):
+#     print(i**3 + 3, end=" ")
+
+# #kolejne
+
+# for i in range(105,1000,15):
+#     print(i, end=" ")
+    
+
+
+    
