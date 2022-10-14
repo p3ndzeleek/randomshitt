be3678e01ea20a797814281aa179d33cc9acb96b
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-14 18:59:46 +0000

    tgnyhbf tghbntg

--- Kartapracy2a.py
+++ Kartapracy2a.py
@@ -16,8 +16,24 @@
 #     print("Nie")
 
 #zad3
-k, l, m = int(input()), int(input()), int(input())
-if (k == l) or (k == m) or (l == m):
-    print("tak") 
-else:
-    print("nie")
+# k, l, m = int(input()), int(input()), int(input())
+# if (k == l) or (k == m) or (l == m):
+#     print("tak") 
+# else:
+#     print("nie")
+
+#zad4
+
+# a, b, c, d, = int(input()), int(input()), int(input()), int(input())
+# if a<b and a<c and a<d:
+#     print("a jest najmniejsze")
+# if b<a and b<c and b<d:
+#     print("b jest najmniejsze")
+# if c<a and c<b and c<d:
+#     print("c jest najmniejsze")
+# if d<a and d<b and d<c:
+#     print("d jest najmnijesze")
+
+#5
+
+a, b, c = int(input()), int(input()), int(input())
