5873465d51197a446d245cb69b764dfd0dc3d969
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-17 11:20:33 +0000

    petle jakies

--- Kartapracy3.py
+++ Kartapracy3.py
@@ -34,7 +34,23 @@
 
 # for i in range(105,1000,15):
 #     print(i, end=" ")
-    
+
+#pętle for liczb trzycyfrowych podzielnych przez 13
+# for i in range(100, 1000):
+#     if i% 13== 0:
+#         print( i, end=" ")
+#petle for liczb dwucyfrowych parzystych
+# for i in range(10, 99, 2):
+#     print( i, end=" ")
+#petle for potęg cyfr: 0, 1, 4, 9, 16, ... 81
+
+# for i in range(10):
+#     print(i**2, end=" ")
+
+n = int(input())
+for i in range(1, n+1):
+    if n % i ==0:
+        print(n, end=" ")
 
 
     
